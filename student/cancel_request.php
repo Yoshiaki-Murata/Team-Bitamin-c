54aab00dedd2a31592b330f13b36ce28862714c8
Keitaro Ogura <arugogura@gmail.com>
2026-04-24 11:44:08 +0900

    fix index, cancel_request,cancel_request_do

--- student/cancel_request.php
+++ student/cancel_request.php
@@ -58,7 +58,7 @@ $methods = $method_stmt->fetchAll(PDO::FETCH_ASSOC);
         </tbody>
       </table>
 
-      <p id="description">キャンセル理由(必須)をご記入下さい。<br>
+      <p id="description">キャンセル理由(必須:50文字以内)をご記入下さい。<br>
         面談方法の変更(対面/zoom)については面談前日午前中までに、LINE又は事務局まで直接お申し出下さい。</p>
 
       <form action="./cancel_request_do.php" method="post" id="cancelForm">
@@ -166,8 +166,8 @@ $methods = $method_stmt->fetchAll(PDO::FETCH_ASSOC);
       openBtn.addEventListener('click', () => {
         const text = textarea.value.trim();
 
-        if (text === "") {
-          alert("入力してください");
+        if (text === "" || text.length > 50) {
+          alert("入力してください（50文字以内）");
           return;
         }
 
