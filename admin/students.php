0fff92464399296b5022e4466bfce92373fe774a
Ehara <mnrehr0913@gmail.com>
2026-04-24 15:11:02 +0900

    エラー改善

--- admin/students.php
+++ admin/students.php
@@ -116,7 +116,7 @@ require_once './../inc/header_admin.php';
   <h1 class="c-title">訓練生一覧</h1>
   <?php if (!empty($_SESSION["msg"])): ?>
     <p class="alert alert-success" role="alert">
-      <?php echo h($_SESSION["msg"]);
+      <?php echo h($_SESSION["msg"]); ?>
     <p class="alert alert-success text-center mx-auto col-6" role="alert">
       <?php echo $_SESSION["msg"];
       unset($_SESSION["msg"]);
@@ -125,7 +125,7 @@ require_once './../inc/header_admin.php';
   <?php endif; ?>
   <?php if (!empty($_SESSION["err_msg"])): ?>
     <p class="alert alert-danger" role="alert">
-      <?php echo h($_SESSION["err_msg"]);
+      <?php echo h($_SESSION["err_msg"]); ?>
     <p class="alert alert-danger text-center mx-auto col-6" role="alert">
       <?php echo $_SESSION["err_msg"];
       unset($_SESSION["err_msg"]);
